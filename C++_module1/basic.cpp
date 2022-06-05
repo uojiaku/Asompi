ff5879f6322b1e8292df2e1f13df1103010aa2b2
ojiakuboss <ojiakuboss@users-MacBook-Pro.local>
2022-06-05 02:15:49 -0400

    added C++

---
+++ C++_module1/basic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     cout << "Ntughari Uche";
+     cout << "\n";
+    return 0;
+   
+}
+
+ 
+
+// g++ -o <name of binary> <source file>
+// ./<name of binary>
