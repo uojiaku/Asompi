5c9dd7507c2129de0fa1e75137360d0d6f612db9
ojiakuboss <ojiakuboss@users-MacBook-Pro.local>
2022-06-07 04:25:44 -0400

    finished js twosum

--- JS_module1/twosum.js
+++ JS_module1/twosum.js
@@ -40,3 +40,30 @@ const twosum2 = (array, targetSum) => {
 }
 
 console.log(twosum2(arr, target))
+
+// O(nLog(n)) time | O(1) Space
+const twosum3 = (array, targetSum) => {
+    array.sort((a, b) => a - b)
+    var left = 0
+    var right = array.length - 1
+    while (left < right){
+       var currentSum = array[left] + array[right]
+    if (currentSum == targetSum) {
+        return [array[left], array[right]]
+    } else if (currentSum < targetSum) {
+        left++
+    } else if (currentSum > targetSum) {
+        right--
+    }
+  }
+  return []
+}
+
+console.log(twosum3(arr, target))
+
+/* 
+   array.sort((a, b) => a - b)
+   negative number -> if a is smaller than b, then a will be sorted to the left of b
+   positive number -> if a is bigger than b, then b will be sorted to the right of b
+   zero happens when a and b are equal, then it doesn't matter whichb one comes first 
+*/
